6fd289213c0cda9293ed2a68de37324e4f08989b
tsulocalize <tsulocalizing@gmail.com>
2025-12-31 19:50:14 +0900

    feat: 디스코드 아이콘 추가

--- src/components/UserCounter.tsx
+++ src/components/UserCounter.tsx
@@ -20,9 +20,19 @@ function UserCounter() {
         return () => clearInterval(intervalId);
     }, []);
 
+    const handleClickDiscord = () => {
+        window.open("https://discord.gg/48J5u2NVwK", "_blank");
+    };
+
     return (
-        <div className="flex items-center justify-center font-sans fixed top-[2%] right-1 bg-transparent text-blue-100 px-3 py-1 text-md font-semibold w-[13%] min-w-[160px] max-w-full h-[5%] whitespace-pre">
+        <div className="flex items-center justify-center font-sans fixed top-[2%] right-1 bg-transparent text-blue-100 px-3 py-1 text-md font-semibold w-[13%] min-w-[160px] max-w-full h-[5%] whitespace-pre gap-2">
             {`현재 접속자 수: ${count}`}
+            <img
+              src="/discord.png"
+              alt="클릭 시 이동"
+              onClick={handleClickDiscord}
+              style={{ width: '20px', cursor: 'pointer' }}
+            />
         </div>
     );
 }
